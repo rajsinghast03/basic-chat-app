f264b6339f52e176637f408d61e477f36a26398a
raj singh <rajsinghast03@gmail.com>
2025-03-08 12:30:30 +0530

    port change

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
